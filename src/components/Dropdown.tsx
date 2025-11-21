3f5449b65f4c6e59bbbaf9d3f5c076f47979c4e1
Abdul Rehman <abdulrehmantahir142@gmail.com>
2025-11-21 22:40:37 +0500

    "feat:dropdown"

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,53 +3,37 @@ interface DropdownProps {
 }
 
 const Dropdown = ({ darkMode }: DropdownProps) => {
+  const services = [
+    "Cleaner",
+    "Electrician",
+    "Plumber",
+    "Mechanic",
+    "Mover",
+    "Technician",
+    "Painter",
+    "Gardener",
+  ];
+
   return (
-    <div className="flex flex-col bg-[#231212] dark:bg-black shadow-lg rounded-md p-2 border-2 dark:border-[#231212]">
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Cleaner
+    <div className={`flex flex-col shadow-lg rounded-md p-2 border-2 ${
+      darkMode 
+        ? "bg-black border-[#231212]" 
+        : "bg-[#231212] border-[#231212]"
+    }`}>
+      {services.map((service, index) => (
+        <div 
+          key={index}
+          className={`border-[#231212] m-1 rounded-md transition-all duration-200 cursor-pointer ${
+            darkMode
+              ? "bg-[#231212] hover:bg-[#422727] text-white"
+              : "bg-white hover:bg-[#422727] text-black"
+          }`}
+        >
+          <div className="font-ibm-plex-mono text-sm p-2 hover:text-white">
+            {service}
+          </div>
         </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm text-black dark:text-white p-2 hover:text-white">
-          Electrician
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm text-black dark:text-white p-2 hover:text-white">
-          Plumber
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm text-black dark:text-white p-2 hover:text-white">
-          Mechanic
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Mover
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Technician
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Painter
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Gardener
-        </div>
-      </div>
-      <div className="border-[#231212] m-1 rounded-md bg-white dark:bg-[#231212] hover:bg-[#422727]">
-        <div className="font-ibm-plex-mono text-sm  text-black dark:text-white p-2 hover:text-white">
-          Mechanic
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
